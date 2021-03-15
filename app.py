d1e9c3cf47fd358f97b97f1e311f38415f414a55
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-03-15 12:50:14 -0300

    refatorando heprimo

--- app.py
+++ app.py
@@ -12,28 +12,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/primos", methods=["GET","POST"])
+@app.route("/nprimos", methods=["GET","POST"])
 def primos():
-    total = request.args.get("valor")
-    qts_primos_tot = int(total)
-    
-    qtds_primo = 1
-    num_primos = [2]
-    numero = 3
 
-    while qtds_primo  < qts_primos_tot:
-        ehprimo = True
-        for i in range(2, numero):
-            if numero % i == 0:
-                ehprimo = False
-                break
-        if (ehprimo):
-            num_primos.append(numero)
-            qtds_primo += 1
-        numero += 1
+    max_qts_primos_total = int(request.args.get("valor"))
+
+    qtds_primo_encontrados = 1
+    num_primos = [2]
+    candidato_primo = 3
+
+    while qtds_primo_encontrados < max_qts_primos_total:
+
+        if (verifica_primo(candidato_primo)):
+            num_primos.append(candidato_primo)
+            qtds_primo_encontrados += 1
+        candidato_primo += 1
     print(num_primos)
 
     return render_template('primos.html', tam = len(num_primos), lista = num_primos)
+
+def verifica_primo(candidato_primo): 
+        ehprimo = True
+        for i in range(2, candidato_primo):
+            if candidato_primo % i == 0:
+                ehprimo = False
+                break
+        return ehprimo    
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT",5000))
